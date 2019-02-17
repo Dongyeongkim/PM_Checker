be875cb34633dd6c64a5b2a42c3745fbd8da34c9
Dongyeongkim <42883224+Dongyeongkim@users.noreply.github.com>
2019-02-17 20:20:33 +0900

    Changing
    Changing the Code Vextractor for simplifing

--- PM/Vextractor.py
+++ PM/Vextractor.py
@@ -8,6 +8,7 @@ Typical usage:
        and saves weights to <new_model_iteration.h5> once complete.
        See data.py for data specifications.
 """
+
 import argparse
 import random, string
 import os
@@ -19,7 +20,6 @@ from keras.models import Model
 import console
 import conversion
 from data import Data
-
 
 
 class AcapellaBot:
@@ -59,29 +59,7 @@ class AcapellaBot:
         # in the middle of the network
         self.peakDownscaleFactor = 4
 
-    def train(self, data, epochs, batch=8):
-        xTrain, yTrain = data.train()
-        xValid, yValid = data.valid()
-        while epochs > 0:
-            console.log("Training for", epochs, "epochs on", len(xTrain), "examples")
-            self.model.fit(xTrain, yTrain, batch_size=batch, epochs=epochs, validation_data=(xValid, yValid))
-            console.notify(str(epochs) + " Epochs Complete!", "Training on", data.inPath, "with size", batch)
-            while True:
-                try:
-                    epochs = int(input("How many more epochs should we train for? "))
-                    break
-                except ValueError:
-                    console.warn("Oops, number parse failed. Try again, I guess?")
-            if epochs > 0:
-                save = input("Should we save intermediate weights [y/n]? ")
-                if not save.lower().startswith("n"):
-                    weightPath = ''.join(random.choice(string.digits) for _ in range(16)) + ".h5"
-                    console.log("Saving intermediate weights to", weightPath)
-                    self.saveWeights(weightPath)
 
-
-    def saveWeights(self, path):
-        self.model.save_weights(path, overwrite=True)
     def loadWeights(self, path):
         self.model.load_weights(path)
     def isolateVocals(self, path, fftWindowSize, phaseIterations=10):
@@ -103,42 +81,8 @@ class AcapellaBot:
         console.log("Converted to audio; writing to", outputFileNameBase)
 
         conversion.saveAudioFile(newAudio,'Test_Acapella/'+outputFileNameBase + ".wav", sampleRate)
-        f = open("Test_Acapella/Acapella.txt",'w'); f.write(outputFileNameBase+".wav"); f.close()
+        conversion.saveSpectrogram(newSpectrogram, 'Test_Image/'+outputFileNameBase + ".png")
         console.log("Vocal isolation complete")
 
-if __name__ == "__main__":
-    # if data folder is specified, create a new data object and train on the data
-    # if input audio is specified, infer on the input
-    parser = argparse.ArgumentParser(description="Acapella extraction with a convolutional neural network")
-    parser.add_argument("--fft", default=1536, type=int, help="Size of FFT windows")
-    parser.add_argument("--data", default=None, type=str, help="Path containing training data")
-    parser.add_argument("--split", default=0.9, type=float, help="Proportion of the data to train on")
-    parser.add_argument("--epochs", default=10, type=int, help="Number of epochs to train.")
-    parser.add_argument("--weights", default="weights.h5", type=str, help="h5 file to read/write weights to")
-    parser.add_argument("--batch", default=8, type=int, help="Batch size for training")
-    parser.add_argument("--phase", default=10, type=int, help="Phase iterations for reconstruction")
-    parser.add_argument("--load", action='store_true', help="Load previous weights file before starting")
-    parser.add_argument("files", nargs="*", default=[])
-
-    args = parser.parse_args()
-
-    acapellabot = AcapellaBot()
-
-    if len(args.files) == 0 and args.data:
-        console.log("No files provided; attempting to train on " + args.data + "...")
-        if args.load:
-            console.h1("Loading Weights")
-            acapellabot.loadWeights(args.weights)
-        console.h1("Loading Data")
-        data = Data(args.data, args.fft, args.split)
-        console.h1("Training Model")
-        acapellabot.train(data, args.epochs, args.batch)
-        acapellabot.saveWeights(args.weights)
-    elif len(args.files) > 0:
-        console.log("Weights provided; performing inference on " + str(args.files) + "...")
-        console.h1("Loading weights")
-        acapellabot.loadWeights(args.weights)
-        for f in args.files:
-            acapellabot.isolateVocals(f, args.fft, args.phase)
-    else:
-        console.error("Please provide data to train on (--data) or files to infer on")
+f = open("Test_Music/Music.txt",'r');path = f.read()
+isolateVocals(path,1536)
