1a470032d6a68e788c3315d22f79f1b0b90c1d6e
Dongyeongkim <42883224+Dongyeongkim@users.noreply.github.com>
2019-02-17 23:32:07 +0900

    Vocal
    Because of Vocal Percentage

--- PM/Segment_Vocal_Ex.py
+++ PM/Segment_Vocal_Ex.py
@@ -1,13 +1,16 @@
 from scipy.io.wavfile import read
 f = open("Test_Acapella/Acapella.txt",'r')
-Acapella = f.read();Acapella1 = read("Test_Acapella/"+Acapella+".wav")
-Acapella1 = numpy.array(a[0], dtype = float)
-for i in range(len(Acapella)):
-    if(Acapella[i] ==0):
-        f = open("Test_Acapella/"+Acapella+".txt",'a')
-        f.close()
-    else:
+Acapella = f.read();Music = Acapella.replace("_acapella","");
+Acapella1 = read("Test_Acapella/"+Acapella+".wav");Music1 = read("Test_Music/"+Music)
+Acapella1 = numpy.array(a[0], dtype = float); Music1 = numpy.array(a[0], dtype = float)
+for i in range(len(Music1)):
+    if(Music1 == 0):
         continue
+    else:
+       Vocal_Percentage = (Acapella1/Music1)*100
+       print(Vocal_Percentage)
+        
+
 
 
 
