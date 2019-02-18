54b571cd99f6a96bd6bb38c0f69fb98079017ea0
Dongyeongkim <42883224+Dongyeongkim@users.noreply.github.com>
2019-02-18 23:15:08 +0900

    Update Segment_Vocal_Ex.py
    Bug is now fixing---

--- PM/Segment_Vocal_Ex.py
+++ PM/Segment_Vocal_Ex.py
@@ -1,8 +1,8 @@
-from scipy.io.wavfile import read
+from scipy.io.wavfile import read; import numpy
 f = open("Test_Acapella/Acapella.txt",'r')
 Acapella = f.read();Music = Acapella.replace("_acapella","");
 Acapella1 = read("Test_Acapella/"+Acapella+".wav");Music1 = read("Test_Music/"+Music)
-Acapella1 = numpy.array(a[0], dtype = float); Music1 = numpy.array(a[0], dtype = float)
+Acapella1 = numpy.array(Acapella1, dtype = int16); Music1 = numpy.array(Music1, dtype = int16)
 for i in range(len(Music1)):
     if(Music1 == 0):
         continue
